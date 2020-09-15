058e1f3cbf996cea665f8a634dfb96ed70a219b0
Filip Margitai <u18.415@pslib.cloud>
2020-09-15 13:28:44 +0200

    GameLogic addded

---
+++ Minesweeper/Minesweeper/GameLogic.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Minesweeper
+{
+    class GameLogic
+    {
+        private int height { get; }
+        private int width { get; }
+        private int bombQuantity { get; }
+        private List<Square> mineField { get; set; }
+        public GameLogic(int _height = 9, int _width = 9, int _bombQuantity = 14)
+        {
+            height = _height;
+            width = _width;
+            bombQuantity = _bombQuantity;
+        }
+
+    }
+}
