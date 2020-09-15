a3a4a1c7413741bc1e3ddf5ef2a87cc0f703321e
Filip Margitai <u18.415@pslib.cloud>
2020-09-15 13:08:03 +0200

    square class

---
+++ Minesweeper/Minesweeper/Square.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Minesweeper
+{
+    class Square
+    {
+        public int x { get; }
+        public int y { get; }
+        public int surrounding { get; set; }
+        public bool isBomb { get; }
+    }
+}
