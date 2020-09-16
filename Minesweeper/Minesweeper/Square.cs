6ecfd8599a67be46f6e06ecb238504681f499eef
Filip Margitai <u18.415@pslib.cloud>
2020-09-17 01:18:07 +0200

    x, y cordinates on square class no longer needed

--- Minesweeper/Minesweeper/Square.cs
+++ Minesweeper/Minesweeper/Square.cs
@@ -6,15 +6,15 @@ namespace Minesweeper
 {
     class Square
     {
-        public int x { get; }
-        public int y { get; }
         public int surrounding { get; set; }
         public bool isBomb { get; }
-        public Square(int _x, int _y, bool _isBomb)
+        public Square(bool _isBomb)
         {
-            x = _x;
-            y = _y;
             isBomb = _isBomb;
+        }
+        public void GetSurrounding()
+        {
+            //TODO
         }
     }
 }
