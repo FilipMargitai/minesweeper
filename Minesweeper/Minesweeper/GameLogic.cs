73abc34050ce6eab9963bb7a357cac618da8423b
Filip Margitai <u18.415@pslib.cloud>
2020-09-17 13:39:11 +0200

    generate field method

--- Minesweeper/Minesweeper/GameLogic.cs
+++ Minesweeper/Minesweeper/GameLogic.cs
@@ -9,12 +9,20 @@ namespace Minesweeper
     {
         private int bombQuantity { get; }
         //private List<Square> mineField { get; set; }
-        private Square[,] mineField { get; set; } 
+        private Square[,] mineField { get; set; }
+        private Random randomNumb { get; set; }
         public GameLogic(int _height = 9, int _width = 9, int _bombQuantity = 14)
         {
             bombQuantity = _bombQuantity;
 
             mineField = new Square[_height, _width];
+        }
+        public void GenerateField()
+        {
+            for (int i = 0; i < bombQuantity; i++)
+            {
+                mineField[randomNumb.Next(mineField.GetLength(0)), randomNumb.Next(mineField.GetLength(1))] = new Square(true);
+            }
         }
         public void PrintArray()
         {
